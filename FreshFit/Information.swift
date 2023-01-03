c55966ca74ea872c98f29c5f904a021af9f7dc11
amertaydin <amertaydin14@gmail.com>
2023-01-03 23:25:09 +0300

    add Information struct to hold users'  firstName and LastName

---
+++ FreshFit/Information.swift
@@ -0,0 +1,13 @@
+//
+//  Information.swift
+//  FreshFit
+//
+//  Created by Mert Aydın on 3.01.2023.
+//
+
+import Foundation
+
+struct Information {
+    var firstName: String
+    var lastName: String
+}
